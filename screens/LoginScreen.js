51b7b39d3c3036b13c0610d6c678d8a9170bad5a
pasztor-andras <andras.pasztor1@gmail.com>
2022-03-18 10:35:00 +0100

    create states and function

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { FontAwesome } from "@expo/vector-icons";
 import { Button, Input } from "react-native-elements";
 
 const LoginScreen = () => {
+
+
+    //-----to input fields----
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    const signIn = () => {
+        console.log("Klikk signIn")
+    }
+
   return (
     <View>
       <StatusBar style="light"></StatusBar>
       <FontAwesome name="user-circle-o" size={100} color="black" />
       <View style={styles.inputContainer}>
-        <Input placeholder="Email" autofocus type="email"/>
-        <Input placeholder="Password" secureTextEntry type="password"/>
+        <Input placeholder="Email" autofocus type="email" value={email} onChangeText={(text) => setEmail(text)}/>
+        <Input placeholder="Password" secureTextEntry type="password" value={password} onChangeText={(text) => setPassword(text)}/>
       </View>
+      <Button title="Login" containerStyle={styles.button} onPress={signIn}/>
+      <Button title="Sing Up" containerStyle={styles.button} type="outline"/>
     </View>
   );
 };
@@ -21,4 +34,9 @@ export default LoginScreen;
 
 const styles = StyleSheet.create({
   inputContainer: {},
+  button:{
+      margin: 20,
+      alignSelf: "center",
+      width: 100,
+  },
 });
