614c7cba801b86f6775cd0ea0da6afadbfdf775f
pasztor-andras <andras.pasztor1@gmail.com>
2022-03-18 10:42:45 +0100

    add register screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, KeyboardAvoidingView } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { FontAwesome } from "@expo/vector-icons";
 import { Button, Input } from "react-native-elements";
@@ -7,7 +7,7 @@ import { Button, Input } from "react-native-elements";
 const LoginScreen = () => {
 
 
-    //-----to input fields----
+    //-----to input fields----//
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
@@ -17,7 +17,7 @@ const LoginScreen = () => {
     }
 
   return (
-    <View>
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
       <StatusBar style="light"></StatusBar>
       <FontAwesome name="user-circle-o" size={100} color="black" />
       <View style={styles.inputContainer}>
@@ -26,17 +26,24 @@ const LoginScreen = () => {
       </View>
       <Button title="Login" containerStyle={styles.button} onPress={signIn}/>
       <Button title="Sing Up" containerStyle={styles.button} type="outline"/>
-    </View>
+    </KeyboardAvoidingView>
   );
 };
 
 export default LoginScreen;
 
 const styles = StyleSheet.create({
-  inputContainer: {},
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 10,
+    },
+  inputContainer: {
+      width: 200,
+  },
   button:{
-      margin: 20,
-      alignSelf: "center",
+      marginTop: 10,
       width: 100,
   },
 });
